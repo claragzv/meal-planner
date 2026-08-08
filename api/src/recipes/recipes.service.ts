83c3d75ddab555c8177655c5a16db9ce3893fef1
claragzv <claragzv@gmail.com>
2026-08-08 23:58:14 +0200

    metodos recipe

--- api/src/recipes/recipes.service.ts
+++ api/src/recipes/recipes.service.ts
@@ -1,35 +1,71 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateRecipeDto } from './dto/create-recipe.dto.js';
 import { UpdateRecipeDto } from './dto/update-recipe.dto.js';
+import { PrismaService } from '../prisma/prisma.service.js';
 
 @Injectable()
 export class RecipesService {
-  private recipes: CreateRecipeDto[] = [];
+  // eso es para que cuando nest cree un recipe service, haga una instancia de prisma service y la inyecte en el constructor
+  constructor(private readonly prisma: PrismaService) { }
 
   create(createRecipeDto: CreateRecipeDto) {
-    const recipe = {
-      id: this.recipes.length + 1,
-      ...createRecipeDto,
-    };
+    return this.prisma.recipe.create({
+      data: createRecipeDto,
+    });
+  }
 
-    this.recipes.push(recipe);
+  findAll() {
+    return this.prisma.recipe.findMany();
+  }
+
+  async findOne(id: number) {
+    const recipe = await this.prisma.recipe.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!recipe) {
+      throw new NotFoundException(`Recipe with id ${id} not found`);
+    }
 
     return recipe;
   }
 
-  findAll() {
-    return this.recipes;
+  async update(id: number, updateRecipeDto: UpdateRecipeDto) {
+    const recipe = await this.prisma.recipe.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!recipe) {
+      throw new NotFoundException(`Recipe with id ${id} not found`);
+    }
+
+    return this.prisma.recipe.update({
+      where: {
+        id,
+      },
+      data: updateRecipeDto,
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} recipe`;
-  }
+  async remove(id: number) {
+    const recipe = await this.prisma.recipe.findUnique({
+      where: {
+        id,
+      },
+    });
 
-  update(id: number, updateRecipeDto: UpdateRecipeDto) {
-    return `This action updates a #${id} recipe`;
-  }
+    if (!recipe) {
+      throw new NotFoundException(`Recipe with id ${id} not found`);
+    }
 
-  remove(id: number) {
-    return `This action removes a #${id} recipe`;
+    return this.prisma.recipe.delete({
+      where: {
+        id,
+      },
+    });
   }
 }
