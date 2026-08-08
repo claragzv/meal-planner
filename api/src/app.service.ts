623a820f3ebee104929ca38a4207b5f65aa9e44c
claragzv <claragzv@gmail.com>
2026-08-08 20:04:07 +0200

    crear controlador, servicios y modulo de recetas

--- api/src/app.service.ts
+++ api/src/app.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 
+// Define que cosas se pueden hacer y el controller las usa
 @Injectable()
 export class AppService {
   getHello(): string {
---
+++ api/src/recipes/recipes.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import { CreateRecipeDto } from './dto/create-recipe.dto';
+import { UpdateRecipeDto } from './dto/update-recipe.dto';
+
+@Injectable()
+export class RecipesService {
+  private recipes: CreateRecipeDto[] = [];
+
+  create(createRecipeDto: CreateRecipeDto) {
+    const recipe = {
+      id: this.recipes.length + 1,
+      ...createRecipeDto,
+    };
+
+    this.recipes.push(recipe);
+
+    return recipe;
+  }
+
+  findAll() {
+    return this.recipes;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} recipe`;
+  }
+
+  update(id: number, updateRecipeDto: UpdateRecipeDto) {
+    return `This action updates a #${id} recipe`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} recipe`;
+  }
+}
